d13533d42438533672784d2a6ea1447b69ac9250
PezeM <pezemos@gmail.com>
2021-07-19 15:06:16 +0200

    Added lobby settings documentation

--- shared/interfaces/lobby.ts
+++ shared/interfaces/lobby.ts
@@ -1,10 +1,33 @@
 import { LobbyLanguage } from "../enums/lobby";
 
 export interface LobbySettings {
+  /**
+   * Maximum number of players in lobby
+   */
   maxPlayers: number;
+
+  /**
+   * Language of the cards
+   */
   language: LobbyLanguage;
+
+  /**
+   * Time of each round in ms
+   */
   roundTime: number;
+
+  /**
+   * Maximum number of card skips per team
+   */
   maximumNumberOfSkips: number;
+
+  /**
+   * Number of points required to win
+   */
   pointsToWin: number;
+
+  /**
+   * IDs of selected cards
+   */
   cardsIds?: string[];
 }
