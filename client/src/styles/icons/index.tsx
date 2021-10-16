ed0ade9a8cfea458616365732534acfee07c8a23
PezeM <pezemos@gmail.com>
2021-10-16 17:08:34 +0200

    Added clock item

--- client/src/styles/icons/index.tsx
+++ client/src/styles/icons/index.tsx
@@ -97,3 +97,21 @@ export const RedoIcon = createIcon({
     <path key={2} d="M15.5 13 19 9.5c-1.3668-1.36683-2.1332-2.13317-3.5-3.5" />,
   ],
 });
+
+export const ClockIcon = createIcon({
+  displayName: 'clockIcon',
+  defaultProps: {
+    fill: 'none',
+    stroke: 'currentColor',
+    strokeWidth: '2',
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+  },
+  path: [
+    <path key={1} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />,
+  ],
+});
+
+// <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+//   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+// </svg>
