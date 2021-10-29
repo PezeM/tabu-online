a0f8fea81a691c568c34290fd7f2350910928a47
PezeM <pezemos@gmail.com>
2021-10-29 21:57:28 +0200

    Added game settings tab

--- client/src/components/LobbySettings/GameSettingsTab.tsx
+++ client/src/components/LobbySettings/GameSettingsTab.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { LobbySettingsContainer } from '@/components/LobbySettings/LobbySettingsContainer';
 import { SettingsBox } from '@/components/LobbySettings/SettingsBox';
-import { PizzaSliceIcon } from '@/styles/icons';
+import { CardsIcon, ClockIcon, GlobeIcon, GroupIcon, PizzaSliceIcon, TrophyIcon } from '@/styles/icons';
 import { ChangeCardsLanguageSelect } from '@/components/LobbySettings/ChangeCardsLanguageSelect';
 import { ChangeMaxPlayersNumberSelect } from '@/components/LobbySettings/ChangeMaxPlayersNumberSelect';
 import { ChangePointsToWinSlider } from '@/components/LobbySettings/ChangePointsToWinSlider';
 import { ChangeMaxSkipsNumberSlider } from '@/components/LobbySettings/ChangeMaxSkipsNumberSlider';
 import { ChangeRoundTimeSlider } from '@/components/LobbySettings/ChangeRoundTimeSlider';
 import { SelectCards } from '@/components/LobbySettings/SelectCards';
+import { RepeatIcon } from '@chakra-ui/icons';
 
 export const GameSettingsTab = React.memo(() => {
   const { t } = useTranslation();
@@ -18,38 +19,38 @@ export const GameSettingsTab = React.memo(() => {
       <SettingsBox
         title={t('ui.maxPlayersNumber')}
         tooltip={t('ui.maxPlayersNumberTooltip')}
-        icon={<PizzaSliceIcon />}
+        icon={<GroupIcon />}
       >
         <ChangeMaxPlayersNumberSelect />
       </SettingsBox>
       <SettingsBox
         title={t('ui.pointsToWin')}
         tooltip={t('ui.pointsToWinTooltip')}
-        icon={<PizzaSliceIcon />}
+        icon={<TrophyIcon />}
       >
         <ChangePointsToWinSlider />
       </SettingsBox>
       <SettingsBox
         title={t('ui.cardsLanguage')}
         tooltip={t('ui.cardsLanguageTooltip')}
-        icon={<PizzaSliceIcon />}
+        icon={<GlobeIcon />}
       >
         <ChangeCardsLanguageSelect />
       </SettingsBox>
-      <SettingsBox title={t('ui.cards')} tooltip={t('ui.cardsTooltip')} icon={<PizzaSliceIcon />}>
+      <SettingsBox title={t('ui.cards')} tooltip={t('ui.cardsTooltip')} icon={<CardsIcon />}>
         <SelectCards />
       </SettingsBox>
       <SettingsBox
         title={t('ui.roundTime')}
         tooltip={t('ui.roundTimeTooltip')}
-        icon={<PizzaSliceIcon />}
+        icon={<ClockIcon />}
       >
         <ChangeRoundTimeSlider />
       </SettingsBox>
       <SettingsBox
         title={t('ui.maxNumberOfSkips')}
         tooltip={t('ui.maxNumberOfSkipsTooltip')}
-        icon={<PizzaSliceIcon />}
+        icon={<RepeatIcon />}
       >
         <ChangeMaxSkipsNumberSlider />
       </SettingsBox>
