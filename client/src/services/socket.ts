83d644bbc90faba21b7dbb7ffdf2915ffb0b21b4
PezeM <16854655+PezeM@users.noreply.github.com>
2021-11-25 19:33:22 +0100

    Removed socket not used code

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,21 +1,12 @@
 import { io, Socket } from 'socket.io-client';
 import { EventsFromClient, EventsFromServer } from '../../../shared/socket';
 
-
-console.log(process.env);
-
 export const socket: Socket<EventsFromServer, EventsFromClient> = io(process.env.REACT_APP_SERVER_URL as string, {
   autoConnect: false
 });
 
-if (process.env.NODE_ENV === 'development') {
-  // const toast = createStandaloneToast({
-  //   theme,
-  //   colorMode: theme.config.initialColorMode
-  // });
-
+if (process.env.NODE_ENV !== 'production') {
   socket.onAny((eventName, ...args) => {
     console.log(`Event: ${eventName} Args:`, args);
-    // showNotification(toast, JSON.stringify(args), { title: eventName, duration: 3000 });
   });
 }
